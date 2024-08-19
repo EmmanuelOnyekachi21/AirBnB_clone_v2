a8ae6f308c1560cdaabe71be4dc0ba4df7d020d9
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-08-19 13:07:52 +0100

    Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+"""Starts a Flask web application.
+
+This script sets up a basic Flask web application,
+ that listens on 0.0.0.0, port 5000,
+and displays the message 'Hello HBNB!' when the root URL is accessed.
+
+Usage:
+    Run this script directly to start the Flask application.
+
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text variable
+
+Return:
+    None
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Displays 'Hello HBNB!' at the root URL."""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns 'HBNB' on the /hbnb route."""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c(text):
+    new_text = ''
+    for char in text:
+        if char == '_':
+            new_text += ' '
+        else:
+            new_text += char
+
+    return f'C {new_text}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
