32bc505960d51b8ec3b91caa8535362cc25dff41
Emmanuel <emmanuelonyekachi04122000@gmail.com>
2024-08-19 12:34:15 +0100

    Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Starts a Flask web application.
+
+This script sets up a basic Flask web application,
+ that listens on 0.0.0.0, port 5000,
+and displays the message 'Hello HBNB!' when the root URL is accessed.
+
+Usage:
+    Run this script directly to start the Flask application.
+
+Routes:
+    /: Displays 'Hello HBNB!'
+
+Return:
+    None
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """Displays 'Hello HBNB!' at the root URL."""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
